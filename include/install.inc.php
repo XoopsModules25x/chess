4eb8b5bc828c268e542a97e9b52010f9ef4d4756
mambax7 <mambax7@gmail.com>
2020-05-25 04:43:22 -0400

    Unnecessary pass-by-ref

--- include/install.inc.php
+++ include/install.inc.php
@@ -126,7 +126,7 @@ function xoops_module_pre_update_chess($module, $oldversion)
  * @param int    $oldversion Old version number of module
  * @return bool                True if update succeeded, otherwise false
  */
-function xoops_module_update_chess(&$module, $oldversion)
+function xoops_module_update_chess($module, $oldversion)
 {
     global $xoopsDB;
 
